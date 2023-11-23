c937b0dd2c573e9d4bfd9f330c6d158d946e3388
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-23 14:45:32 -0600

    add stripe config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 const CONFIG = {
   JWT_SECRET: process.env["JWT_SECRET"] || "",
-  STRAPI_URL: process.env["STRAPI_URL"] || ""
+  STRAPI_URL: process.env["STRAPI_URL"] || "",
+  STRIPE_SECRET: process.env["STRIPE_SECRET"] || "",
 };
 
 export default CONFIG;
