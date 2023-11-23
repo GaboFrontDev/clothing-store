5ea630cc81fd12958283bbfe8c4873cdc83db507
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-23 10:19:18 -0600

    refactor of request

--- src/contexts/shared/infrastructure/StrapiRepository.ts
+++ src/contexts/shared/infrastructure/StrapiRepository.ts
@@ -1,11 +1,13 @@
+import CONFIG from "@/config";
+
 export class StrapiRepository<T> {
     protected endpoint: string;
     constructor(endpoint: string) {
         this.endpoint = endpoint;
     }
 
-    protected async get(): Promise<T[]> {
-        const res = await fetch(this.endpoint);
+    protected async get(query?:string): Promise<T[]> {
+        const res = await fetch(`${this.endpoint}${query}` );
         if (!res.ok) {
             throw new Error('Query failed');
         }
@@ -13,7 +15,7 @@ export class StrapiRepository<T> {
     }
 
     protected async getByQuery(query: string): Promise<T> {
-        const res = await fetch(`${this.endpoint}?${query}`);
+        const res = await this.request(`${this.endpoint}?${query}`);
         if (!res.ok) {
             throw new Error('Query failed');
         }
@@ -22,13 +24,7 @@ export class StrapiRepository<T> {
 
 
     protected async create(payload: string): Promise<T> {
-        const res = await fetch(`${this.endpoint}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: payload
-        });
+        const res = await this.request(`${this.endpoint}`, 'POST', payload);
         if (!res.ok) {
             throw new Error('Create failed');
         }
@@ -36,17 +32,29 @@ export class StrapiRepository<T> {
     }
 
     protected async update(payload: string, id: string): Promise<T> {
-        const res = await fetch(`${this.endpoint}/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: payload
-        });
+        const res = await this.request(`${this.endpoint}/${id}`, 'PUT', payload);
         if (!res.ok) {
             throw new Error('Create failed');
         }
         return await res.json() as T;
+    }
+
+    private async request(url: string, method: 'GET' | 'POST' | 'PUT' = 'GET', payload?: string, ) {
+        const options = {
+            method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+        if(method === 'GET') {
+            return fetch(`${CONFIG.STRAPI_URL}/${url}` , {
+                ...options,
+            });
+        }
+        return fetch(url, {
+            ...options,
+            body: payload
+        });
     }
 
 
