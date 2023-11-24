f0b1a706d062be42721c973801c3c3c98663b6b3
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-23 23:40:20 -0600

    add do payment

--- src/contexts/payment/application/PaymentController.ts
+++ src/contexts/payment/application/PaymentController.ts
@@ -1,10 +1,11 @@
 import { PaymentStripePayloadEntity } from "../domain/PaymentPayloadEntity";
+import PaymentRepository from "../infrastructure/PaymentRepository";
 
 export class PaymentController {
     constructor() {
     }
 
     async makePayment(data: PaymentStripePayloadEntity){
-
+        PaymentRepository.doPayment()
     }
 }
