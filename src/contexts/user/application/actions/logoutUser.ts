6dbb1b9594da33631a24b7791f1c316272bc3aea
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-23 23:56:12 -0600

    fix logout

--- src/contexts/user/application/actions/logoutUser.ts
+++ src/contexts/user/application/actions/logoutUser.ts
@@ -3,6 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 export async function logoutAction(req: NextRequest) {
     try {
         req.cookies.delete('auth_token');
+        return NextResponse.json(
+            {
+                sucess: true,
+            },
+            {
+                status: 200,
+            }
+        );
     } catch (error) {
         return NextResponse.json(
             {
