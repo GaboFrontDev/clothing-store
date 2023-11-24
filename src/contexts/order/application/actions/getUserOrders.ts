a2a2f844292f8068f177c1c5a51dddccc3d6d0e9
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-23 23:56:26 -0600

    refactor and lint

--- src/contexts/order/application/actions/getUserOrders.ts
+++ src/contexts/order/application/actions/getUserOrders.ts
@@ -5,29 +5,29 @@ import { OrderSearchPayloadEntity } from "../../domain/OrderSearchPayloadEntity"
 const { getOrdersByUserId } = new OrderController<NextRequest>();
 
 export async function getUserOrdersAction(req: NextRequest) {
-    try {
-        const payload = await req.json() as OrderSearchPayloadEntity;
-        
-        const orders = await getOrdersByUserId(req, payload.id);
-        return NextResponse.json(
-            {
-                sucess: true,
-                result: {
-                    orders
-                }
-            },
-            {
-                status: 200,
-            }
-        );
-    } catch (error) {
-        return NextResponse.json(
-            {
-                sucess: false,
-            },
-            {
-                status: 500,
-            }
-        );
-    }
+  try {
+    const { id: userId } = (await req.json()) as OrderSearchPayloadEntity;
+
+    const orders = await getOrdersByUserId(req, userId);
+    return NextResponse.json(
+      {
+        sucess: true,
+        result: {
+          orders,
+        },
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        sucess: false,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
