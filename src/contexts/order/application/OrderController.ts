585e263860d758c70310435cb0eea18d5e0b7678
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-24 01:09:54 -0600

    update orders

--- src/contexts/order/application/OrderController.ts
+++ src/contexts/order/application/OrderController.ts
@@ -1,13 +1,22 @@
 import { authMiddleware } from "@/decorators/authorized";
 import OrderRepository from "../infrastructure/OrderRepository";
 import { OrderEntity } from "../domain/OrderEntity";
+import { randomUUID } from "crypto";
 
 export class OrderController<Request> {
   @authMiddleware
-  getOrdersByUserId(req: Request, id: string): Promise<OrderEntity> {
+  async getOrdersByUserId(req: Request, id: string): Promise<OrderEntity> {
     return OrderRepository.getOrdersByUserId(id);
   }
 
   @authMiddleware
-  addToCart(req: Request) {}
+  async addToCart(req: Request) {}
+
+  @authMiddleware
+  async createOrder(req: Request, payload: Omit<OrderEntity, "id">) {
+    OrderRepository.createNewOrder({
+      ...payload,
+      id: randomUUID(),
+    });
+  }
 }
