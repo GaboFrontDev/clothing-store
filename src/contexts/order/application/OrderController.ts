fcb655ced3c304818f5309a63615c6e8d1248d09
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-23 23:40:13 -0600

    refactor functions

--- src/contexts/order/application/OrderController.ts
+++ src/contexts/order/application/OrderController.ts
@@ -2,11 +2,12 @@ import { authMiddleware } from "@/decorators/authorized";
 import OrderRepository from "../infrastructure/OrderRepository";
 import { OrderEntity } from "../domain/OrderEntity";
 
-export class OrderController<Request> { 
-    @authMiddleware
-    getOrdersByUserId(req: Request, id: string): Promise<OrderEntity> {
-        return OrderRepository.getOrdersByUserId(id);
+export class OrderController<Request> {
+  @authMiddleware
+  getOrdersByUserId(req: Request, id: string): Promise<OrderEntity> {
+    return OrderRepository.getOrdersByUserId(id);
+  }
 
-    }
-
+  @authMiddleware
+  addToCart(req: Request) {}
 }
