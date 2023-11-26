fd6b9bf87b74618ebea96d21021eaf18916dc9c4
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-25 19:39:48 -0600

    refactor request

--- src/contexts/shared/infrastructure/StrapiRepository.ts
+++ src/contexts/shared/infrastructure/StrapiRepository.ts
@@ -1,4 +1,4 @@
-import CONFIG from "@/config";
+import { request } from "./utils/request";
 
 export class StrapiRepository<T> {
     protected endpoint: string;
@@ -15,7 +15,7 @@ export class StrapiRepository<T> {
     }
 
     protected async getByQuery(query: string): Promise<T> {
-        const res = await this.request(`${this.endpoint}?${query}`);
+        const res = await request(`${this.endpoint}?${query}`);
         if (!res.ok) {
             throw new Error('Query failed');
         }
@@ -24,7 +24,7 @@ export class StrapiRepository<T> {
 
 
     protected async create(payload: string): Promise<T> {
-        const res = await this.request(`${this.endpoint}`, 'POST', payload);
+        const res = await request(`${this.endpoint}`, 'POST', payload);
         if (!res.ok) {
             throw new Error('Create failed');
         }
@@ -32,31 +32,13 @@ export class StrapiRepository<T> {
     }
 
     protected async update(payload: string, id: string): Promise<T> {
-        const res = await this.request(`${this.endpoint}/${id}`, 'PUT', payload);
+        const res = await request(`${this.endpoint}/${id}`, 'PUT', payload);
         if (!res.ok) {
             throw new Error('Create failed');
         }
         return await res.json() as T;
     }
 
-    private async request(url: string, method: 'GET' | 'POST' | 'PUT' = 'GET', payload?: string, ) {
-        const options = {
-            method,
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${CONFIG.STRAPI_TOKEN}`
-            },
-        };
-        if(method === 'GET') {
-            return fetch(`${CONFIG.STRAPI_URL}/${url}` , {
-                ...options,
-            });
-        }
-        return fetch(url, {
-            ...options,
-            body: payload
-        });
-    }
 
 
 }
---
+++ src/contexts/shared/infrastructure/utils/request.ts
@@ -0,0 +1,20 @@
+import CONFIG from "@/config";
+
+export function request(url: string, method: 'GET' | 'POST' | 'PUT' = 'GET', payload?: string, ) {
+    const options = {
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${CONFIG.STRAPI_TOKEN}`
+        },
+    };
+    if(method === 'GET') {
+        return fetch(`${CONFIG.STRAPI_URL}/${url}` , {
+            ...options,
+        });
+    }
+    return fetch(url, {
+        ...options,
+        body: payload
+    });
+}
