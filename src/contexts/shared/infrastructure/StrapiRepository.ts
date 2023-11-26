69584ed654e1a453d39e3dbb45ea9746f248a08e
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-25 19:40:23 -0600

    rename error messages

--- src/contexts/shared/infrastructure/StrapiRepository.ts
+++ src/contexts/shared/infrastructure/StrapiRepository.ts
@@ -17,7 +17,7 @@ export class StrapiRepository<T> {
     protected async getByQuery(query: string): Promise<T> {
         const res = await request(`${this.endpoint}?${query}`);
         if (!res.ok) {
-            throw new Error('Query failed');
+            throw new Error(`Query ${query} failed`);
         }
         return await res.json() as T;
     }
@@ -34,7 +34,7 @@ export class StrapiRepository<T> {
     protected async update(payload: string, id: string): Promise<T> {
         const res = await request(`${this.endpoint}/${id}`, 'PUT', payload);
         if (!res.ok) {
-            throw new Error('Create failed');
+            throw new Error('Update failed');
         }
         return await res.json() as T;
     }
