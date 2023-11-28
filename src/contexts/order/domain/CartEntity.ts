2198255ac2cdc668a9ba20c6da46390d4d8277c2
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-28 00:07:29 -0600

    rename cart entity

--- src/contexts/order/domain/CartEntity.ts
+++ src/contexts/order/domain/CartEntity.ts
@@ -1,6 +1,6 @@
 import { ProductEntity } from "@/contexts/product/domain/ProductEntity";
 
-export interface OrderEntity {
+export interface CartEntity {
     products: ProductEntity[];
     user_id: string;
     amount: number;
