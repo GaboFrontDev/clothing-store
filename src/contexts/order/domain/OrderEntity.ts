892359e3c35b04944cb3b2a6fe83cc904dc17f15
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-28 00:03:02 -0600

    fix order entity imports

--- src/contexts/order/domain/OrderEntity.ts
+++ src/contexts/order/domain/OrderEntity.ts
@@ -1,5 +1,4 @@
 import { ProductEntity } from "@/contexts/product/domain/ProductEntity";
-import { UserEntity } from "@/contexts/user/domain/UserEntity";
 
 export interface OrderEntity {
     id: string;
