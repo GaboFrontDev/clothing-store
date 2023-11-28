48a45a648ef7042efec35738e70584c95d59312c
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-28 00:02:46 -0600

    add and remove products from cart

--- src/contexts/order/application/OrderController.ts
+++ src/contexts/order/application/OrderController.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@/decorators/authorized";
 import OrderRepository from "../infrastructure/OrderRepository";
 import { OrderEntity } from "../domain/OrderEntity";
 import { randomUUID } from "crypto";
+import { ProductEntity } from "@/contexts/product/domain/ProductEntity";
 
 export class OrderController<Request> {
   @authMiddleware
@@ -9,8 +10,28 @@ export class OrderController<Request> {
     return OrderRepository.getOrdersByUserId(id);
   }
 
-  @authMiddleware
-  async addToCart(req: Request) {}
+  async addToCart(cart: OrderEntity, newProduct: ProductEntity, amount = 1) {
+    const index = cart.products.map(product => product.id).indexOf(newProduct.id);
+    if(index >= 0) {
+      cart.products[index].amount += amount;
+    } else {
+      cart.products.push(newProduct);
+    }
+    cart.amount += newProduct.price;
+
+  }
+
+  async removeFromCart(cart: OrderEntity, productId: string, deleteAll = false) {
+    const index = cart.products.map(product => product.id).indexOf(productId);
+    if(index < 0){
+      throw Error('Product not in cart');
+    }
+    if(deleteAll) {
+      cart.products.splice(index, 1);
+      return 0;
+    }
+    cart.products[index].amount -= 1;
+  }
 
   @authMiddleware
   async createOrder(req: Request, payload: Omit<OrderEntity, "id">) {
