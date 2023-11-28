18bfbe263483d969d8bade36260f318f52c838b0
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-28 00:15:33 -0600

    make cart to be returned from repository

--- src/contexts/order/application/OrderController.ts
+++ src/contexts/order/application/OrderController.ts
@@ -3,6 +3,7 @@ import OrderRepository from "../infrastructure/OrderRepository";
 import { OrderEntity } from "../domain/OrderEntity";
 import { randomUUID } from "crypto";
 import { ProductEntity } from "@/contexts/product/domain/ProductEntity";
+import { CartEntity } from "../domain/CartEntity";
 
 export class OrderController<Request> {
   @authMiddleware
@@ -10,7 +11,7 @@ export class OrderController<Request> {
     return OrderRepository.getOrdersByUserId(id);
   }
 
-  async addToCart(cart: OrderEntity, newProduct: ProductEntity, amount = 1) {
+  async addToCart(cart: CartEntity, newProduct: ProductEntity, amount = 1) {
     const index = cart.products.map(product => product.id).indexOf(newProduct.id);
     if(index >= 0) {
       cart.products[index].amount += amount;
@@ -18,10 +19,10 @@ export class OrderController<Request> {
       cart.products.push(newProduct);
     }
     cart.amount += newProduct.price;
-
+    return cart;
   }
 
-  async removeFromCart(cart: OrderEntity, productId: string, deleteAll = false) {
+  async removeFromCart(cart: CartEntity, productId: string, deleteAll = false) {
     const index = cart.products.map(product => product.id).indexOf(productId);
     if(index < 0){
       throw Error('Product not in cart');
@@ -31,6 +32,7 @@ export class OrderController<Request> {
       return 0;
     }
     cart.products[index].amount -= 1;
+    return cart;
   }
 
   @authMiddleware
