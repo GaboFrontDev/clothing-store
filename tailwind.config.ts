036f560fdc8304f1f9fb8a7d0681d32aa314f4da
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-11-30 12:05:01 -0600

    update colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,14 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'store-bg':{
+        100: "#f6f6f6",
+        200: "",
+        300: "",
+        400: "",
+      }
+    }
   },
   plugins: [],
 }
