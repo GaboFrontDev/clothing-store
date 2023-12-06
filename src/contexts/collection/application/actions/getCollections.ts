8dbd3dd0f68845b50fa132058758a907a205dc56
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-05 22:55:20 -0600

    fixes get collection action

--- src/contexts/collection/application/actions/getCollections.ts
+++ src/contexts/collection/application/actions/getCollections.ts
@@ -1,21 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import { CollectionCotroller } from "../CollectionController";
 
-const {getCollections} = new CollectionCotroller<NextRequest>();
+const { getCollections } = new CollectionCotroller<NextRequest>();
 
-export async function getCollectionsAction(req: NextRequest) {
+export async function getCollectionsAction(req?: NextRequest) {
     try {
-        const data = await getCollections();
+        const collections = await getCollections();
+        console.log(`Found collections:`)
+        console.log(collections)
+        if (!req) {
+            return collections;
+        }
         return NextResponse.json(
             {
                 sucess: true,
-                result: data
+                result: collections
             },
             {
                 status: 201,
             }
         );
     } catch (error) {
+        console.log(`getCollections error: ${error}`);
+        if (!req) {
+            
+            return [];
+        }
+
         return NextResponse.json(
             {
                 sucess: false,
