3c0515c304158bde89c44cfdd65f7f6c3c6afc64
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-10 00:06:33 -0600

    fix styles

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -1,11 +1,24 @@
 import { Route } from "@/consts/interfaces";
+import { twMerge } from "tailwind-merge";
 
 interface NavigationButtonProps {
-    route: Route
+  route: Route;
+  className?: string;
 }
 
-export function NavigationButton(props: NavigationButtonProps) {
-    const { route: { url, displayValue } } = props;
+const DEFAULT_CLASSES = "block hover:bg-store-bg-100 text-4xl bold";
 
-    return <a className="block" href={url}>{displayValue}</a>
+export function NavigationButton(props: NavigationButtonProps) {
+  const {
+    route: { url, displayValue },
+    className = "",
+  } = props;
+
+  const classes = twMerge(DEFAULT_CLASSES, className);
+
+  return (
+    <a className={classes} href={url}>
+      {displayValue}
+    </a>
+  );
 }
