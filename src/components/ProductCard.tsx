031da9770e23d614bd50edf26d6fc0c7e50782a4
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-11 16:34:03 -0600

    makes usage of button

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { ProductEntity } from "@/contexts/product/domain/ProductEntity";
 import { StrapiEntryEntity } from "@/contexts/shared/domain/StrapiEntity";
 import { PhotoVisualize } from "./PhotoVisualize";
+import Buttons from "./Buttons";
 
 interface ProductCardProps {
   product: StrapiEntryEntity<ProductEntity>;
@@ -21,12 +22,12 @@ export default function ProductCard(props: ProductCardProps) {
       <section className="p-4 flex items-center flex-col border border-slate-500 border-t-0 rounded-b-lg">
         <p>{product.attributes.name}</p>
         <p>${product.attributes.price} MXN</p>
-        <a
+        <Buttons.Link
           href=""
           className="border-2 border-emerald-800 active:bg-emerald-800 active:text-white hover:bg-emerald-800 hover:text-white rounded-md px-2"
         >
           Agregar al carrito
-        </a>
+        </Buttons.Link>
       </section>
     </div>
   );
