bae866fd60a94d7b078e37829f09de863c443906
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-12 00:07:33 -0600

    add href to overlay

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 
 interface OverlayProps {
-    children: React.ReactNode
+  children: React.ReactNode;
+  href?: string;
 }
 
 export function Overlay(props: OverlayProps) {
-    const { children } = props;
+    const { children, href } = props;
     return <div className="group hidden absolute h-full w-full hover:bg-gray-800 hover:bg-opacity-60 transition-colors md:flex flex-row justify-center items-center">
         <a
             className="hidden group-hover:flex transition-all  items-center bg-store-bg-100 px-2 py-1 mx-2"
-            href=""
+            href={href}
         >
             {children}
         </a>
