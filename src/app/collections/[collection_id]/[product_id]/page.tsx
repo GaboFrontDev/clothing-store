f8aba90e32d15b89c9080ba32108def74110b612
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-12 00:27:02 -0600

    add go back

--- src/app/collections/[collection_id]/[product_id]/page.tsx
+++ src/app/collections/[collection_id]/[product_id]/page.tsx
@@ -5,6 +5,7 @@ import { getProductById } from "@/contexts/cart/application/helpers/productsInve
 interface PageProps {
   params: {
     product_id: string;
+    collection_id: string;
   };
 }
 
@@ -12,44 +13,51 @@ export default async function CollectionProductIdPage(
   props: PageProps
 ) {
   const {
-    params: { product_id },
+    params: { product_id, collection_id },
   } = props;
   const product = await getProductById(
     product_id
   );
-  console.log(product);
 
   return (
-    <section className="my-6 md:flex">
-      <section className="md:max-w-[40vw]">
-        <Carousel
-          images={product.attributes.photos.data}
-        />
+    <>
+      <Buttons.Link href={`/collections/${collection_id}`} className="bg-transparent">Go back</Buttons.Link>
+
+      <section className="my-6 md:flex">
+        <section className="md:max-w-[40vw]">
+          <Carousel
+            images={
+              product.attributes.photos.data
+            }
+          />
+        </section>
+        <section className="md:m-2">
+          <div className="m-4">
+            <p className="font-medium text-xl">
+              {product.attributes.name}
+            </p>
+          </div>
+          <div className="m-4">
+            <p>
+              {product.attributes.description}
+            </p>
+          </div>
+          <div className="m-4">
+            <p className="font-medium text-xl">
+              $
+              {product.attributes.price.toLocaleString(
+                "en-US"
+              )}
+            </p>
+          </div>
+          <Buttons.Link
+            href=""
+            className=" text-center border-2 border-emerald-800 active:bg-emerald-800 active:text-white hover:bg-emerald-800 hover:text-white rounded-md px-2 block md:inline-block"
+          >
+            Add to cart
+          </Buttons.Link>
+        </section>
       </section>
-      <section className="md:m-2">
-        <div className="m-4">
-          <p className="font-medium text-xl">
-            {product.attributes.name}
-          </p>
-        </div>
-        <div className="m-4">
-          <p>{product.attributes.description}</p>
-        </div>
-        <div className="m-4">
-          <p className="font-medium text-xl">
-            $
-            {product.attributes.price.toLocaleString(
-              "en-US"
-            )}
-          </p>
-        </div>
-        <Buttons.Link
-          href=""
-          className=" text-center border-2 border-emerald-800 active:bg-emerald-800 active:text-white hover:bg-emerald-800 hover:text-white rounded-md px-2 block md:inline-block"
-        >
-          Add to cart
-        </Buttons.Link>
-      </section>
-    </section>
+    </>
   );
 }
