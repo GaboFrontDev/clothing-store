c0addaeb8653491017ce1166f43c80bfdb135dd7
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-11 23:41:14 -0600

    add arrow icons

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -4,3 +4,5 @@ export { ShoppingBag } from "./ShoppingBag";
 export { Menu } from "./Menu";
 export { Facebook } from "./Facebook";
 export { Instagram } from "./Instagram";
+export { ArrowLeft } from "./ArrowLeft";
+export { ArrowRight } from "./ArrowRight";
