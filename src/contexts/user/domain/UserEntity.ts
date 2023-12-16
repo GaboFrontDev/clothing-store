c754b37c31a65825a513275f6ff5dfae80d5ee16
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-16 12:11:29 -0600

    update type and fix style

--- src/contexts/user/domain/UserEntity.ts
+++ src/contexts/user/domain/UserEntity.ts
@@ -1,30 +1,32 @@
-import { StrapiEntryEntity } from "@/contexts/shared/domain/StrapiEntity"
+import { StrapiEntryEntity } from "@/contexts/shared/domain/StrapiEntity";
 
 export interface UserCredentialsPayloadEntity {
-    password: string,
-    salt: string,
-    verification_token?: string
+  password: string;
+  salt: string;
+  verification_token?: string;
+  uuid?: string;
 }
 
 export interface UserCredentialsEntity {
-    password: string,
-    salt: string,
-    verification_token?: string
+  password: string;
+  salt: string;
+  verification_token?: string;
+  uuid?: string;
 }
 export interface UserEntity {
-    first_name: string,
-    last_name: string,
-    address: string,
-    phone: string
-    verified: boolean,
-    email: string,
-    user_credential: StrapiEntryEntity<UserCredentialsEntity>
+  first_name: string;
+  last_name: string;
+  address: string;
+  phone: string;
+  verified: boolean;
+  email: string;
+  user_credential: StrapiEntryEntity<UserCredentialsEntity>;
 }
 
 export interface UserPayloadEntity
   extends Omit<UserEntity, "user_credential"> {}
 
-  export interface UserStrapiPayloadEntity
+export interface UserStrapiPayloadEntity
   extends Omit<UserEntity, "user_credential"> {
-    user_credential: number;
-  }
+  user_credential: number;
+}
