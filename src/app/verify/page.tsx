024726d7571377528d20d27f16df3b5da2c53f27
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-16 12:16:38 -0600

    refactor code

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,11 +1,41 @@
+import Buttons from "@/components/Buttons";
+import { validateUserAction } from "@/contexts/user/application/actions/validateUser";
+
 interface PageProps {
-    searchParams: {
-      t: string;
-    };
+  searchParams: {
+    i: string;
+    r: string
+  };
+}
+
+export default async function VerifyPage(
+  props: PageProps
+) {
+  const {
+    searchParams: { i: credentialUUID },
+  } = props;
+  try {
+    const {user, result: validated} = (await validateUserAction(credentialUUID));
+    if(!validated) {
+      return (
+        <>
+          Link has expired, please{" "}
+          <Buttons.Link
+            href={`/verify?r${user.data[0].id}`}
+          >
+            click here
+          </Buttons.Link>{" "}
+          to receive another one
+        </>
+      );
+
+    }
+
+    
+  } catch (error) {
+    console.log("Error happened at token validation");
+
   }
-  
 
-export default function VerifyPage(props: PageProps){
-    const {searchParams: {t: token} } = props;
-
+  return <></>;
 }
