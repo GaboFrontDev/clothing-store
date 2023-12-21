aabf1c6ebe5a709a4e66df65fa9f6ca81728bda8
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-21 03:16:53 -0600

    remove cache

--- src/contexts/shared/infrastructure/utils/request.ts
+++ src/contexts/shared/infrastructure/utils/request.ts
@@ -7,12 +7,16 @@ export function strapiRequest(url: string, method: 'GET' | 'POST' | 'PUT' = 'GET
             'Content-Type': 'application/json; charset=utf-8',
             Authorization: `Bearer ${CONFIG.STRAPI_TOKEN}`
         },
+        cache: "no-store" as any
     };
     if(method === 'GET') {
         console.log(`Calling GET: ${CONFIG.STRAPI_URL}/${url}`);        
-        return fetch(`${CONFIG.STRAPI_URL}/${url}` , {
+        return fetch(
+          `${CONFIG.STRAPI_URL}/${url}`,
+          {
             ...options,
-        });
+          }
+        );
     }
     console.log(`Calling ${method}: ${CONFIG.STRAPI_URL}/${url}`);
     console.log(`Payload: ${payload}`);
