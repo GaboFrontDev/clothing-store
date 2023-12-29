02a65e7d32fc2d5b35e7e7a80cd9475e35db99f4
Gabriel Hinojoza <39060802+GaboFrontDev@users.noreply.github.com>
2023-12-28 22:28:01 -0600

    hide social

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import { Facebook, Instagram } from "./Icon";
 export default function Footer() {
   return (
     <div className="flex flex-grow-1 justify-evenly">
-      <a href="https://www.facebook.com/hdzlpaloma" target="_blank">
+      {/* <a href="https://www.facebook.com/hdzlpaloma" target="_blank">
         <Facebook />
       </a>
       <a href="https://www.instagram.com/hdzlpaloma/" target="_blank">
         <Instagram />
-      </a>
+      </a> */}
     </div>
   );
 }
